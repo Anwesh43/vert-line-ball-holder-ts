8f717f28184a9c7774ef048ccba6220bfc44a1ba
Anwesh43 <anweshthecool0@gmail.com>
2021-04-26 09:52:33 +0530

    adding Animator class

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ const colors : Array<string> = [
     "#795548"
 ] 
 const backColor : string = "#bdbdbd"
+const delay : number = 20 
 
 class ScaleUtil {
 
@@ -128,6 +129,26 @@ class State {
         if (this.dir == 0) {
             this.dir = 1 - 2 * this.prevScale 
             cb()
+        }
+    }
+}
+
+class Animator {
+
+    animated : boolean = false 
+    interval : number 
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false  
+            clearInterval(this.interval)
         }
     }
 }
