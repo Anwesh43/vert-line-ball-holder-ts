d35e071c6b964aa66e4010ea6629c7816b34e554
Anwesh43 <anweshthecool0@gmail.com>
2021-04-26 09:47:22 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -28,3 +28,51 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawVertLineBallHolder(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const r : number = Math.min(w, h) / rFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-size, 0)
+            context.rotate(sf2 * Math.PI / 2)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * sf1)
+            context.restore()
+            DrawingUtil.drawCircle(context, -size + r, -h / 2 + r + (h / 2 - 2 * r) * sf4, r * sf1)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawVLBHNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawVertLineBallHolder(context, scale)
+    }
+}
