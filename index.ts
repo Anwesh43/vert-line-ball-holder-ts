67e94369077fcdeaaaf0aa4a11378878c6d0e60b
Anwesh43 <anweshthecool0@gmail.com>
2021-04-26 09:59:20 +0530

    adding VertLineBallHolder class

--- index.ts
+++ index.ts
@@ -194,3 +194,26 @@ class VLBHNode {
         return this 
     }
 }
+
+class VertLineBallHolder {
+
+    curr : VLBHNode = new VLBHNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
